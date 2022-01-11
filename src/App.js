dc9542504d28d2e2f90199a4062201240b9d35be
Fatimah Sarwar <fatimahsarwar@fatimahs-mbp.myfiosgateway.com>
2022-01-11 18:41:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  
   render() {
     return(<div>
-      Github Card
+     <h1> GITHUB INFO </h1>
+     <form>
+       <input />
+       <button>Search</button>
+     </form>
+     <img src = "https://avatars.githubusercontent.com/u/91710091?v=4" />
+     <a href = "https://github.com/FatimahSarwar"><h2>Fatimah Sarwar</h2></a>
+     <h3>Total Repos: 32</h3>
+     <h3>Total followers:</h3>
     </div>);
   }
 }
